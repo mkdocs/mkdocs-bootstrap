4aa6a45e67b6b42a831da522fc86850924f39bdb
Dougal Matthews <dougal@redhat.com>
2015-05-19 01:06:42 +0100

    Initial import

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+
+setup(
+    name="mkdocs-bootstrap",
+    version=VERSION,
+    url='http://www.mkdocs.org',
+    license='BSD',
+    description='Bootstrap theme for MkDocs',
+    author='Dougal Matthews',
+    author_email='dougal@dougalmatthews.com.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'bootstrap = mkdocs_bootstrap',
+        ]
+    },
+    zip_safe=False
+)
